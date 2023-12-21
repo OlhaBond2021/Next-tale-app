3f325d1ea5afc8d3aa7d7e3b27d57b07268ba3c4
OlhaBond2021 <96439245+OlhaBond2021@users.noreply.github.com>
2023-12-21 16:11:38 +0200

    cleanup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', //дає можливість перетворити динамічний код на статичний
-  basePath: '/next-tale-app',
 };
 
 module.exports = nextConfig;
